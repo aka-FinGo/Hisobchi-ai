138a726e14933392b9e8b26c674a98b22f8c045a
aka-FinGo <akafingo@gmail.com>
2026-01-25 23:14:17 +0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.moliya.boshqaruv',
-  appName: 'Moliya Ilovasi',
-  webDir: 'dist',
-  server: {
-    androidScheme: 'https'
-  }
+  appId: 'com.aka.fingo.hisobchi', // O'zingizga moslang
+  appName: 'Hisobchi AI',
+  webDir: 'dist', // 'www' emas, aynan 'dist' bo'lishi kerak
+  bundledWebRuntime: false
 };
 
 export default config;
