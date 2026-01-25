b392f4e3dc58da841977d6201c1fd4fbbd82bde8
aka-FinGo <akafingo@gmail.com>
2026-01-25 23:32:35 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: './', // Build vaqtida yo'llar to'g'ri shakllanishi uchun
+  build: {
+    outDir: 'dist',
+  }
 });
