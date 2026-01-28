1b75597ed5df6a9e60699a77a2f60ac2fb3f070a
aka-FinGo <akafingo@gmail.com>
2026-01-28 08:21:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Hisobchi-ai/', // <--- MANA SHU QATORNI QO'SHING! to'g'ri shakllanishi uchun
+  // OLDINGI: base: '/Hisobchi-ai/',
+  // YANGI (APK uchun to'g'risi):
+  base: './', 
   build: {
     outDir: 'dist',
   }
-});
+})
